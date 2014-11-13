96eec303177ed17f79ee8d7d872f43b7fde00c30
Bill Tihen <btihen@gmail.com>
2014-11-13 17:56:56 +0100

    added plot 2 -- baltimore emissions pattern

---
+++ plot2.R
@@ -0,0 +1,32 @@
+library("data.table")
+
+NEI_df <- readRDS("summarySCC_PM25.rds")
+SCC_df <- readRDS("Source_Classification_Code.rds")
+
+NEI_dt = data.table(NEI_df)
+
+plot_dt <- NEI_dt[ fips == 24510, sum(Emissions)/1000, by=year]
+setnames(plot_dt, "V1", "pollutants")
+
+
+graph <- function(data_dt) {
+  fit <- lm(pollutants ~ year, data=data_dt)
+  slope = round(fit$coef[2], digits=3)
+
+  plot(data_dt, main="Total Emissions of PM2.5 Pollutants in Baltimore over Time", 
+    ylab="PM2.5 Emissions (Kilotons)",ylim=c(0.0,5.0),xlim=c(1998,2009), pch=20)
+  lines(data_dt$year, data_dt$pollutants)
+  abline(fit, col="blue")
+    legend("topright", 
+    legend = paste(c("Slope = ", slope ), collapse = " "),
+    lty=1, # gives the legend appropriate symbols (lines)
+    col="blue")
+
+}
+
+graph(plot_dt)
+
+png(filename = "plot2.png", width = 480, height = 480)
+graph(plot_dt)
+dev.off()
+
