1e99ed611760f81e1151a74f00f7323b178ede3a
Bill Tihen <btihen@gmail.com>
2014-11-13 18:09:15 +0100

    added and formatted the change rate on graph 1

--- plot1.R
+++ plot1.R
@@ -8,10 +8,25 @@ NEI_dt = data.table(NEI_df)
 plot_dt <- NEI_dt[,sum(Emissions)/1000000, by=year]
 setnames(plot_dt, "V1", "pollutants")
 
-graph <- function(data) {
-  plot(data, main="Total US Emissions of PM2.5 Pollutants over Time", ylab="PM2.5 Emissions (Megatons)",ylim=c(3.0,8.0),xlim=c(1998,2009), pch=20)
-  lines(data$year, data$pollutants)
+graph <- function(data_dt) {
+  fit <- lm(pollutants ~ year, data=data_dt)
+  slope = round(fit$coef[2], digits=3)
+
+  plot(data_dt, main="Total US Emissions of PM2.5 Pollutants over Time",
+    ylab="PM2.5 Emissions (Megatons)",ylim=c(3.0,8.0),xlim=c(1998,2009), 
+    pch=20)
+  lines(data_dt$year, data_dt$pollutants)
+  abline(fit, col="blue")
+  
+  lines(data_dt$year, data_dt$pollutants)
+  abline(fit, col="blue")
+    legend("topright", 
+    legend = paste(c("Change = ", slope, "Megatons/year" ), collapse = " "),
+    lty=1, # gives the legend appropriate symbols (lines)
+    col="blue")
 }
+
+graph(plot_dt)
 
 png(filename = "plot1.png", width = 480, height = 480)
 graph(plot_dt)
