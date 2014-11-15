2103a1ff4a71240e622f405adb7529aa00b094b7
Bill Tihen <btihen@gmail.com>
2014-11-15 06:52:25 +0100

    first draft of plot 3 - altnernate approach

---
+++ plot3_alt.R
@@ -0,0 +1,39 @@
+library("ggplot2")
+library("data.table")
+
+NEI_df <- readRDS("summarySCC_PM25.rds")
+SCC_df <- readRDS("Source_Classification_Code.rds")
+
+NEI_dt = data.table(NEI_df)
+
+plot      <- NEI_dt[ fips == 24510, sum(Emissions)/1000, by=list(year,type)])
+setnames(plot, "V1", "pollutants")
+setnames(plot, "type", "source")
+
+text_label <- function( dt, match ) {
+  dt <- dt[ source==match ]
+  fit <- lm(pollutants ~ year, data=dt)
+  slope = round(fit$coef[2], digits=3)
+  text = paste(c("Change = ", slope, "Tons/year" ), collapse = " ")
+}
+
+graph <- function(dt) {
+
+  g <- ggplot( dt, aes(year, pollutants, color=source) )
+  p <- g + geom_point(size=2.5)
+  #p <- g + geom_point( aes(color = source), size=3.5, alpha=4/5)   
+  p <- p + geom_smooth(method ="lm", se=FALSE, size=1.5) 
+  p <- p + scale_colour_discrete(name  ="Source")
+  p <- p + ylab("PM2.5 Emissions (Kilotons)") 
+  p <- p + xlab("Years Pollutants Measured")  
+  p <- p + ggtitle( "Baltimore Emissions of PM2.5 Pollutants by Source over Time\n") 
+  #p <- p + geom_text(x=2005,y=2100, label=text_label(dt,"POINT"), size=4, family="serif", fontface="plain", color=factor("POINT") )
+  #p <- p + geom_text(x=2005,y=2000, label=text_label(dt,"NONPOINT"), size=4, family="serif", fontface="plain", color="green" )
+
+  print(p)
+}
+graph(plot)
+
+png(filename = "plot3_alt.png", width = 480, height = 480)
+graph(plot)
+dev.off()
